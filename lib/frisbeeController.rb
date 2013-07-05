b0564cc4a635969ed2849f8c25169621969145f0
Aris Dadoukis <ardadouk@gmail.com>
2013-07-05 18:44:32 +0300

    bug fix

--- lib/frisbeeController.rb
+++ lib/frisbeeController.rb
@@ -349,6 +349,7 @@ module OmfRc::ResourceProxy::ImagezipClient #Imagezip client
 
     host = Net::Telnet.new("Host" => node[:node_ip], "Timeout" => 60, "Prompt" => /[\w().-]*[\$#>:.]\s?(?:\(enable\))?\s*$/)
     host.cmd(command.to_s) do |c|
+      puts c
       if c !=  "\n" #&& (c[0,8] == "Progress" || c[0,5] == "Wrote")
         client.inform(:status, {
           status_type: 'IMAGEZIP',
