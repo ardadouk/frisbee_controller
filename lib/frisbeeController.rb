b7a5fceb11cd4d4913a3e924a06b3a34d6ea44a8
Aris Dadoukis <ardadouk@gmail.com>
2013-07-15 15:26:32 +0300

    improved messaging

--- lib/frisbeeController.rb
+++ lib/frisbeeController.rb
@@ -390,7 +390,7 @@ module OmfRc::ResourceProxy::ImagezipClient #Imagezip client
 
     host = Net::Telnet.new("Host" => node[:node_ip], "Timeout" => false)#, "Prompt" => /[\w().-]*[\$#>:.]\s?(?:\(enable\))?\s*$/)
     host.cmd(command.to_s) do |c|
-      if c !=  "\n" && c[0,4] != "/usr"
+      if c !=  "\n" && c[0,4] != "/usr" && (c != "." || c != "..")
         client.inform(:status, {
           status_type: 'IMAGEZIP',
           event: "STDOUT",
